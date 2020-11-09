9064eec5207446f9cb24a585297f9fe3b811b894
vincizLe <56980240+vincizLe@users.noreply.github.com>
2020-11-09 11:28:39 -0500

    Implementation of all entities

---
+++ RestCode_WebApplication/Resources/SaveProductResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestCode_WebApplication.Resources
+{
+    public class SaveProductResource
+    {
+        [Required]
+        [MaxLength(50)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public double ProductPrice { get; set; }
+
+        [Required]
+        public double CategoryId { get; set; }
+    }
+}
